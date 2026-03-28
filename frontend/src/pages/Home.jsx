48958d6e9ed96563fff849952ae6cff3b676b549
Coder-Pallab <duarahpallab0@gmail.com>
2026-03-28 18:42:05 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+
+const Home = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Home
