998b113737c2173b1d934df3bf069827ea7fcd75
Coder-Pallab <duarahpallab0@gmail.com>
2026-04-07 20:07:22 +0530

    vercel configuration

---
+++ backend/inngest/index.js
@@ -0,0 +1,56 @@
+import { Inngest } from "inngest";
+import User from "../models/User.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+// inngest function to Save UserData to database
+const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clerk',
+        triggers: [{ event: "clerk/user.created" }],
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.create(userData)
+    }
+)
+
+// Inngest function to Delete User from database
+const syncUserDeletion = inngest.createFunction(
+    {
+        id: 'delete-user-with-clerk',
+        triggers: [{ event: "clerk/user.deleted" }],
+    },
+    async ({ event }) => {
+        const { id } = event.data
+        await User.findByIdAndDelete(id)
+    }
+)
+
+// Inngest function to Update User from database
+const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-from-clerk',
+        triggers: [{ event: "clerk/user.updated" }],
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import dns from 'dns';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 // Setting the DNS server
 dns.setServers(['1.1.1.1', '8.8.8.8'])
@@ -18,9 +21,11 @@ await connectDB();
 // Defining Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 // API Routes
 app.get('/', (req, res) => res.send("Server is up and running!"));
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 // Running the server
 app.listen(port, () => console.log(`Server is up and running on port http://localhost:${port}`));
