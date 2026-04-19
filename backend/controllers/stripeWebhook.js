f1145556981e768605d49349b070e0c9e9815b34
Coder-Pallab <duarahpallab0@gmail.com>
2026-04-19 15:00:22 +0530

    some error fixes

--- backend/controllers/stripeWebhook.js
+++ backend/controllers/stripeWebhook.js
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 import Booking from '../models/Booking.js';
 import Show from '../models/Show.js';
+import { inngest } from '../inngest/index.js';
 
 export const stripeWebhooks = async (req, res) => {
     const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
